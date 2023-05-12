527e27cf655504546181244a7bb5339ecb84261a
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-05-12 10:50:44 -0400

    added delete button to delete defective files

---
+++ client/src/DeletePatient.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+function DeletePatient({patient, patients, setPatients}) {
+    const [wantToDelete, setWantToDelete] = useState(false)
+    function deleteRecord(){
+        fetch(`/patients/${patient.id}`, {
+            method: 'DELETE'
+        })
+        setPatients(patients.filter(element=>element.id!==patient.id))
+    }
+
+//   if (patient.chart) {return null}
+  return (
+    <div>
+        <button onClick={()=>setWantToDelete(true)}>
+            Delete {patient.first_name} {patient.last_name} chart: {patient.chart ? patient.chart.id: 'none'}
+        </button>
+        <br/>
+        {!wantToDelete ? null : 
+        <div>
+            <p>Are you sure?</p>
+            <button onClick={()=>deleteRecord()}>Yes</button>  
+            <button onClick={()=>setWantToDelete(false)}>No</button>
+        </div>}
+
+    </div>
+  )
+}
+
+export default DeletePatient
