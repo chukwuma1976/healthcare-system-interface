0704ff325f211331c92cf51f824f349551e221b8
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-05-13 17:23:06 -0400

    still debugging routing error

--- client/src/DisplayPatientRecords.js
+++ client/src/DisplayPatientRecords.js
@@ -10,24 +10,24 @@ import { UserContext } from './User'
 import DeletePatient from './DeletePatient'
 
 function DisplayPatientRecords({patient}) {
-    const {displayDateAsNumbers, patients, setPatients} = useContext(UserContext)
-    const [display, setDisplay] = useState(false)
-    const {id, first_name, last_name, birth_date, age} = patient
-    const [chart, setChart] = useState({
-        history_and_physicals: '',
-        progress_notes: '',
-        discharge_notes: '',
-        consults: '',
-        operative_reports: '',
-        procedure_notes: '',
-    })
-    const {history_and_physicals, progress_notes, discharge_notes, consults, operative_reports, procedure_notes} = chart
+    // const {displayDateAsNumbers, patients, setPatients} = useContext(UserContext)
+    // const [display, setDisplay] = useState(false)
+    // const {id, first_name, last_name, birth_date, age} = patient
+    // const [chart, setChart] = useState({
+    //     history_and_physicals: '',
+    //     progress_notes: '',
+    //     discharge_notes: '',
+    //     consults: '',
+    //     operative_reports: '',
+    //     procedure_notes: '',
+    // })
+    // const {history_and_physicals, progress_notes, discharge_notes, consults, operative_reports, procedure_notes} = chart
     
-    useEffect(()=>{
-        fetch(`/patients/${id}/charts`)
-        .then(res=>res.json())
-        .then(setChart)
-    }, [])
+    // useEffect(()=>{
+    //     fetch(`/patients/${id}/charts`)
+    //     .then(res=>res.json())
+    //     .then(setChart)
+    // }, [])
 
     // if (!patient.chart) return null
   return (
