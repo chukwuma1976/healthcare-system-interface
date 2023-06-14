df8a1960965301f4ee33def9055cde8b32791645
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-06-14 16:58:52 -0400

    readded delete patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-Provider.all
-Patient.all
-Appointment.all
-Chart.all
-Consult.all
-DischargeNote.all
-HistoryAndPhysical.all
-OperativeReport.all
-Picture.all
-ProcedureNote.all
-ProgressNote.all
+# Provider.all
+# Patient.all
+# Appointment.all
+# Chart.all
+# Consult.all
+# DischargeNote.all
+# HistoryAndPhysical.all
+# OperativeReport.all
+# Picture.all
+# ProcedureNote.all
+# ProgressNote.all
 
 
 
-puts "Done seeding patients"
+# puts "Done seeding patients"
